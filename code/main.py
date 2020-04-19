736eda01661c2c74c8c26128e0c642e32850957f
Gerard Heredia <xgerard96@gmail.com>
2020-04-19 18:39:09 +0200

    New repository

---
+++ code/main.py
@@ -0,0 +1,21 @@
+''' Live capture using webcam '''
+
+
+import cv2
+
+
+CAP = cv2.VideoCapture(0)
+if not CAP.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    RET, FRAME = CAP.read()
+    FRAME = cv2.resize(FRAME, None, fx=1, fy=1,
+                       interpolation=cv2.INTER_AREA)
+
+    cv2.imshow('Live face detection', FRAME)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+CAP.release()
+cv2.destroyAllWindows()
